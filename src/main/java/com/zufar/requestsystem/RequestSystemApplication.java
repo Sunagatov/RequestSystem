1445e66fa0ab7d5e49ca544e14e1347ec9d164be
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-03-23 19:52:07 +0400

    Initial commit

---
+++ src/main/java/com/zufar/requestsystem/RequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zufar.requestsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestSystemApplication.class, args);
+    }
+
+}
